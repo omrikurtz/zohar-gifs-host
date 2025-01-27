c1ecc0aa024686f8bdad43a46396d870aaf23433
omrikurtz <omri.kurtz@forter.com>
2025-01-27 23:53:41 +0200

    hara

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/zohar-gifts-host/',
+  base: '/zohar-gifs-host/',
 })
