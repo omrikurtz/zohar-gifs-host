a6d2fb13ebb0a9eb2d322145e706e83356d5558b
omrikurtz <omri.kurtz@forter.com>
2025-02-19 00:54:19 +0200

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MenuMemoryGame from './Game.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename='/zohar-gifs-host'>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/game" element={<MenuMemoryGame />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 );
