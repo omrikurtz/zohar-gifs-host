17c82b455a0ef3eccedce5ab66e8cbca0cfc6f0d
omrikurtz <omri.kurtz@forter.com>
2025-02-19 00:57:59 +0200

    no dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
     content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
     theme: {
       extend: {
